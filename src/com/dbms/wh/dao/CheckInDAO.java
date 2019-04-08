b201b5a7d5e178429358dfee1746a6a862aaf12d
objectivespy08 <ssbhoyar@gmail.com>
2019-04-08 17:51:18 -0400

    Add: Checkin

--- src/com/dbms/wh/dao/CheckInDAO.java
+++ src/com/dbms/wh/dao/CheckInDAO.java
@@ -1,5 +1,111 @@
 package com.dbms.wh.dao;
 
-public class CheckInDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+//import java.util.Date;
 
+import com.dbms.wh.bean.CheckIn;
+
+public class CheckInDAO {
+	public static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/cagarwa3";
+	public static final String user = "cagarwa3";
+	public static final String password = "200234585";
+	Connection connection = null;
+	Statement statement = null;
+	ResultSet result = null;
+
+	public CheckInDAO() {
+
+	}
+
+	public void createCheckIn(CheckIn checkin) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			try {
+				connection = DriverManager.getConnection(jdbcURL, user, password);
+				statement = connection.createStatement();
+				java.sql.Date startDate = new java.sql.Date(checkin.getStartDate().getTime());
+				java.sql.Date endDate = checkin.getEndDate() != null ? new java.sql.Date(checkin.getEndDate().getTime()) : null;
+				statement.executeUpdate("INSERT INTO checkins (patient_id, staff_id, start_date, end_date) VALUES"
+						+ "(" + checkin.getPatientId() + "," + checkin.getStaffId() + ",'" + startDate + "','"
+						+ endDate + "')");
+				System.out.println("New checkin added successfully!");
+			} finally {
+				close(result);
+				close(statement);
+				close(connection);
+			}
+		} catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+	}
+
+	public void updateCheckIn(CheckIn checkin) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			try {
+				connection = DriverManager.getConnection(jdbcURL, user, password);
+				statement = connection.createStatement();
+				java.sql.Date startDate = new java.sql.Date(checkin.getStartDate().getTime());
+				java.sql.Date endDate = checkin.getEndDate() != null ? new java.sql.Date(checkin.getEndDate().getTime()) : null;
+				
+				statement.executeUpdate("UPDATE checkins SET patient_id = " + checkin.getPatientId() + ", staff_id = " + checkin.getStaffId()
+						+ ", start_date = '" + startDate + "', end_date = '" + endDate + "'  WHERE id = " + checkin.getId() + ";");
+				System.out.println("Checkin updated successfully!");
+			} finally {
+				close(result);
+				close(statement);
+				close(connection);
+			}
+		} catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+	}
+
+	public void deleteCheckIn(int checkinId) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			try {
+				connection = DriverManager.getConnection(jdbcURL, user, password);
+				statement = connection.createStatement();
+				statement.executeUpdate("DELETE from checkins where id = " + checkinId + ";");
+				System.out.println("Checkin deleted successfully!");
+			} finally {
+				close(result);
+				close(statement);
+				close(connection);
+			}
+		} catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+	}
+
+	static void close(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	static void close(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	static void close(ResultSet result) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
 }
--- src/com/dbms/wh/servlet/CheckInServlet.java
+++ src/com/dbms/wh/servlet/CheckInServlet.java
@@ -1,40 +1,57 @@
 package com.dbms.wh.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CheckInServlet
- */
+import com.dbms.wh.bean.CheckIn;
+import com.dbms.wh.dao.CheckInDAO;
+
 @WebServlet("/CheckInServlet")
 public class CheckInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CheckInServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public CheckInServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		CheckInDAO checkindao = new CheckInDAO();
+		PrintWriter out = response.getWriter();
+		// String id = request.getParameter("id");
+		try {
+			if (request.getParameter("operation").equals("create")) {
+				// Converting date from Java to SQL
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+				Date startDate = format.parse(request.getParameter("start_date"));
+				Date endDate = request.getParameter("end_date") != null ? format.parse(request.getParameter("end_date"))
+						: null;
+				CheckIn checkin = new CheckIn(Integer.parseInt(request.getParameter("patient_id")),
+						Integer.parseInt(request.getParameter("staff_id")), startDate, endDate);
+				checkindao.createCheckIn(checkin);
+			} else if (request.getParameter("operation").equals("update")) {
+
+			} else if (request.getParameter("operation").equals("delete")) {
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
