6744fcea58127e1670a28043b3db8b71fcf50ff9
objectivespy08 <ssbhoyar@gmail.com>
2019-04-08 15:28:48 -0400

    Add:Staff servlet

---
+++ src/com/dbms/wh/bean/Staff.java
@@ -0,0 +1,105 @@
+package com.dbms.wh.bean;
+
+public class Staff {
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private Integer age;
+	private Integer phone_no;
+	private String name;
+	private String gender;
+	private String job_title;
+	private String professional_title;
+	private String address;
+	private String department;
+	
+	public Staff(String name, Integer age, String gender, String job_title, String professional_title, Integer phone_no, String address, String department) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.job_title = job_title;
+		this.professional_title = professional_title;
+		this.phone_no = phone_no;
+		this.address = address;
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Staff [id=" + id + ", age=" + age + ", name=" + name + ", gender=" + gender + ", phone_no=" + phone_no
+				+ ", job_title=" + job_title + ", professional_title=" + professional_title + ", address=" + address + ",department=" + department + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+//
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJobTitle() {
+		return job_title;
+	}
+
+	public void setJobTitle(String job_title) {
+		this.job_title = job_title;
+	}
+
+	public Integer getPhoneNo() {
+		return phone_no;
+	}
+
+	public void setPhone_no(Integer phone_no) {
+		this.phone_no = phone_no;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getProfessionalTitle() {
+		return professional_title;
+	}
+
+	public void setProfessionalTitle(String professional_title) {
+		this.professional_title = professional_title;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+}
---
+++ src/com/dbms/wh/dao/StaffDAO.java
@@ -0,0 +1,65 @@
+package com.dbms.wh.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.dbms.wh.bean.Staff;
+
+public class StaffDAO {
+	public static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/cagarwa3";
+	public static final String user = "cagarwa3";
+	public static final String password = "200234585";
+	Connection connection = null;
+	Statement statement = null;
+	ResultSet result = null;
+	public StaffDAO() {
+		
+	}
+	public void createStaff(Staff staff) {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			try {
+				connection = DriverManager.getConnection(jdbcURL, user, password);
+				statement = connection.createStatement();
+				statement.executeUpdate(
+							"INSERT INTO staff(name, age, gender, job_title, professional_title, phone_no, address, department) VALUES ('"
+									+ staff.getName() + "', " + staff.getAge() + ", '" + staff.getGender() + "', '" + staff.getJobTitle() + "', '" + staff.getProfessionalTitle()
+									+ "', " + staff.getPhoneNo() + ", '" + staff.getAddress() + "', '" + staff.getDepartment() + "');");
+				System.out.println("New Staff added successfully!");
+			} finally {
+				close(result);
+				close(statement);
+				close(connection);
+			}
+		} catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+	}
+	
+
+	static void close(Connection connection) {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+	static void close(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+	static void close(ResultSet result) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+}
