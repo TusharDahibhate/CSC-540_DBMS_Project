8c887a1c82d255923a1ea7d0b03cad845e38b927
objectivespy08 <ssbhoyar@gmail.com>
2019-04-11 01:10:23 -0400

    Add: jsp staff

---
+++ WebContent/staff-list.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Staff</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/css/foundation.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.5.3/dist/js/foundation.min.js"></script>
+</head>
+<body>
+    <div style="text-align:center">
+         <h2>Staff Management</h2>
+         <form action="StaffServlet" method="get" enctype="multipart/form-data">
+             <input type="submit" class="button" value="Add new">
+             <input type="hidden" name="operation" value="ADD" />
+         </form>
+    
+         <form action="StaffServlet" method="get" enctype="multipart/form-data">
+             <input type="submit" class="button" value="List all">
+             <input type="hidden" name="operation" value="LIST" />
+         </form>
+    </div>
+
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Job Title</th>
+            <th>Professional Title</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Department</th>
+        </tr>
+        <c:forEach var="staff" items="${listStaff}">
+            <tr>
+                <td><c:out value="${staff.id}" /></td>
+                <td><c:out value="${staff.name}" /></td>
+                <td><c:out value="${staff.age}" /></td>
+                <td><c:out value="${staff.gender}" /></td>
+                <td><c:out value="${staff.jobtitle}" /></td>
+                <td><c:out value="${staff.professionaltitle}" /></td>
+                <td><c:out value="${staff.phoneno}" /></td>
+                <td><c:out value="${staff.address}" /></td>
+                <td><c:out value="${staff.department}" /></td>
+                <td>
+                    <form action="StaffServlet" method="get"
+                        enctype="multipart/form-data">
+                        <button type="submit" name="button" value="EDIT"><a>Edit</a></button>
+                        <input type="hidden" name="operation" value="EDIT" />
+                        <input type="hidden" name="id" value='${staff.id}' />
+                    </form>
+                </td>
+                <td>
+                    <form action="StaffServlet" method="get"
+                        enctype="multipart/form-data">
+                        <button type="submit" name="button" value="DELETE"><a>Delete</a></button>
+                        <input type="hidden" name="operation" value="DELETE" />
+                        <input type="hidden" name="id" value='${staff.id}' />
+                    </form>
+                </td>
+            </tr>
+        </c:forEach>
+    </table>
+    
+</body>
+</html>
--- src/com/dbms/wh/dao/StaffDAO.java
+++ src/com/dbms/wh/dao/StaffDAO.java
@@ -2,10 +2,13 @@ package com.dbms.wh.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.dbms.wh.bean.Patient;
 import com.dbms.wh.bean.Staff;
 
 public class StaffDAO {
@@ -16,96 +19,130 @@ public class StaffDAO {
 	Statement statement = null;
 	ResultSet result = null;
 
+	private static final String INSERT_STAFF_SQL = "INSERT INTO staff(name, age, gender, job_title, professional_title, phone_no, address, department) VALUES " + " (?, ?, ?, ?, ?, ?, ?, ?);";
+	private static final String SELECT_STAFF_BY_ID = "SELECT * FROM staff where id =?";
+	private static final String SELECT_ALL_STAFF = "SELECT * FROM staff";
+	private static final String DELETE_STAFF_SQL = "DELETE FROM staff WHERE id = ?;";
+	private static final String UPDATE_STAFF_SQL = "UPDATE staff SET name = ?,age= ?, gender = ?, job_title = ?, professional_title = ?, phone_no = ?, address = ?, department = ? WHERE id = ?;";
+	
 	public StaffDAO() {
 
 	}
 
-	public void createStaff(Staff staff) {
+	protected Connection getConnection() {
+		Connection connection = null;
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			try {
-				connection = DriverManager.getConnection(jdbcURL, user, password);
-				statement = connection.createStatement();
-				statement.executeUpdate(
-						"INSERT INTO staff(name, age, gender, job_title, professional_title, phone_no, address, department) VALUES ('"
-								+ staff.getName() + "', " + staff.getAge() + ", '" + staff.getGender() + "', '"
-								+ staff.getJobTitle() + "', '" + staff.getProfessionalTitle() + "', "
-								+ staff.getPhoneNo() + ", '" + staff.getAddress() + "', '" + staff.getDepartment()
-								+ "');");
-				System.out.println("New Staff added successfully!");
-			} finally {
-				close(result);
-				close(statement);
-				close(connection);
-			}
-		} catch (Throwable oops) {
-			oops.printStackTrace();
+			connection = DriverManager.getConnection(jdbcURL, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	public void insertStaff(Staff staff) throws SQLException {
+		
+		try (Connection connection = getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(INSERT_STAFF_SQL)) {
+			preparedStatement.setString(1, staff.getName());
+			preparedStatement.setInt(2, staff.getAge());
+			preparedStatement.setString(3, staff.getGender());
+			preparedStatement.setString(4, staff.getJobtitle());
+			preparedStatement.setString(5, staff.getProfessionaltitle());
+			preparedStatement.setInt(6, staff.getPhoneno());
+			preparedStatement.setString(7, staff.getAddress());
+			preparedStatement.setString(8, staff.getDepartment());
+			
+
+			System.out.println(preparedStatement);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			printSQLException(e);
 		}
 	}
 
-	public void updatePatient(Staff st) {
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			try {
-				connection = DriverManager.getConnection(jdbcURL, user, password);
-				statement = connection.createStatement();
-				statement.executeUpdate("UPDATE staff SET name = '" + st.getName() + "', age = " + st.getAge()
-						+ ", gender = '" + st.getGender() + "', job_title = '" + st.getJobTitle()
-						+ "', professional_title = '" + st.getProfessionalTitle() + "', phone_no = " + st.getPhoneNo()
-						+ ", address = '" + st.getAddress() + "', department='" + st.getDepartment() + "'  WHERE id = "
-						+ st.getId() + ";");
-				System.out.println("Staff updated successfully!");
-			} finally {
-				close(result);
-				close(statement);
-				close(connection);
+	public Staff selectStaff(int id) {
+		Staff staff = null;
+
+		try (Connection connection = getConnection();
+
+			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_STAFF_BY_ID);) {
+			preparedStatement.setInt(1, id);
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				staff = new Staff(rs.getString("name"), rs.getInt("age"), rs.getString("gender"), rs.getString("job_title"), rs.getString("professional_title"), rs.getInt("phone_no"), rs.getString("address"), rs.getString("department"));
+				staff.setId(id);
 			}
-		} catch (Throwable oops) {
-			oops.printStackTrace();
+		} catch (SQLException e) {
+			printSQLException(e);
 		}
+		return staff;
+	}
+	
+	public List<Staff> selectAllStaff() {
+
+		List<Staff> staff = new ArrayList<>();
+
+		try (Connection connection = getConnection();
+
+			PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_STAFF);) {
+			ResultSet rs = preparedStatement.executeQuery();
+			Staff s;
+			while (rs.next()) {
+				s = new Staff(rs.getString("name"), rs.getInt("age"), rs.getString("gender"), rs.getString("job_title"), rs.getString("professional_title"), rs.getInt("phone_no"), rs.getString("address"), rs.getString("department"));
+				s.setId(rs.getInt("id"));
+				staff.add(s);
+			}
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return staff;
+	}
+	
+	public boolean deleteStaff(int id) throws SQLException {
+		boolean rowDeleted;
+		try (Connection connection = getConnection();
+			PreparedStatement statement = connection.prepareStatement(DELETE_STAFF_SQL);) {
+			statement.setInt(1, id);
+			rowDeleted = statement.executeUpdate() > 0;
+		}
+		return rowDeleted;
 	}
 
-	public void deleteStaff(int staff_id) {
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			try {
-				connection = DriverManager.getConnection(jdbcURL, user, password);
-				statement = connection.createStatement();
-				statement.executeUpdate("DELETE from staff where id = " + staff_id + ";");
-				System.out.println("Staff deleted successfully!");
-			} finally {
-				close(result);
-				close(statement);
-				close(connection);
-			}
-		} catch (Throwable oops) {
-			oops.printStackTrace();
+	public boolean updateStaff(Staff staff) throws SQLException {
+		boolean rowUpdated;
+		try (Connection connection = getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_STAFF_SQL);) {
+			preparedStatement.setString(1, staff.getName());
+			preparedStatement.setInt(2, staff.getAge());
+			preparedStatement.setString(3, staff.getGender());
+			preparedStatement.setString(4, staff.getJobtitle());
+			preparedStatement.setString(5, staff.getProfessionaltitle());
+			preparedStatement.setInt(6, staff.getPhoneno());
+			preparedStatement.setString(7, staff.getAddress());
+			preparedStatement.setString(8, staff.getDepartment());
+			rowUpdated = preparedStatement.executeUpdate() > 0;
 		}
+		return rowUpdated;
 	}
-
-	static void close(Connection connection) {
-		if (connection != null) {
-			try {
-				connection.close();
-			} catch (Throwable whatever) {
-			}
-		}
-	}
-
-	static void close(Statement statement) {
-		if (statement != null) {
-			try {
-				statement.close();
-			} catch (Throwable whatever) {
-			}
-		}
-	}
-
-	static void close(ResultSet result) {
-		if (result != null) {
-			try {
-				result.close();
-			} catch (Throwable whatever) {
+	
+	private void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
 			}
 		}
 	}
